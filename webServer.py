b2e8350ea203861fa1fe4375046105a1480656c5
patrick rolon <pnr7962@nyu.edu>
2026-02-14 01:17:05 -0500

    Updated code to add HTML body to end of header

--- webServer.py
+++ webServer.py
@@ -9,47 +9,46 @@ def webServer(port=13331):
   serverSocket = socket.socket(AF_INET, SOCK_STREAM)
   
   #Prepare a server socket
-  serverSocket.bind(("", port))
+
   #Fill in start
+  serverSocket.bind(("", port))
   serverSocket.listen(1)
   #Fill in end
 
   while True:
     #Establish the connection
-
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
     print(f"Connected by {addr}")
+
     try:
-      message = connectionSocket.recv(1024).decode('utf-8')
+      message = connectionSocket.recv(1024).decode()
       filename = message.split()[1]
       #opens the client requested file.
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:],'rb',).read() #fill in start              #fill in end   )
-      #print(f)
-
-
-
-      #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
+      f = open(filename[1:],'rb').read() #fill in start              #fill in end   )
+       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
 
 
       header = (
               b"HTTP/1.1 200 OK\r\n"
               b"Content-Type: text/html; charset=UTF-8\r\n"
-              b"Content-Length: " + str(len(f)).encode() +
+              b"Content-Length: " + str(len(f)).encode("utf-8") +
               b"\r\n"
               b"\r\n"  # blank line = end of headers
+
       )
 
       appended = header + f
 
-      connectionSocket.sendall(appended)
+
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
  
       #Fill in end
                
       #for lines in f:
+
 
       # #for line in file
       #Fill in start - append your html file contents #Fill in end 
@@ -60,10 +59,10 @@ def webServer(port=13331):
 
       # Fill in start
       connectionSocket.sendall(appended)
-
+      connectionSocket.close()  # closing the connection socket
       # Fill in end
         
-      connectionSocket.close() #closing the connection socket
+
       
     except Exception as e:
       # Send response message for invalid request due to the file not being found (404)
@@ -72,7 +71,6 @@ def webServer(port=13331):
       badResponse = (
         b"HTTP/1.1 404 Not Found\r\n"
         b"Content-Type: text/plain\r\n"
-        b"Content-Length: " + str(len(f)).encode()+
         b"\r\n\r\n"
       )
       connectionSocket.sendall(badResponse)
